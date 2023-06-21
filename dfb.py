91e149b1007222f71d08a337bc1b85612e6b23c1
IshaJD <22ishaarora08@gmail.com>
2023-06-21 08:09:29 +0530

    This the Tourism website

---
+++ dfb.py
@@ -0,0 +1,21 @@
+# import math
+# a=int(input("enter the number betwee 0 to 999:"))
+# if a<10:
+#   print("a is single digit number")
+# elif a<100:
+#     print("a is double digit number")
+# else:
+#     print("a is three digit number")
+    
+# #g=int(input("enter the value of which you want cube"))
+# for i in range(15,20):
+#  print("cube of number ",i,"is" ,i**3)
+#  for i in range(1,10,2):
+#     print("square root of the number is:",(i**0.5))
+a=int(input("enter the valu of a:"))
+b=int(input("enter the value of b"))
+product=0
+for i in range(0,b):
+    product+=a
+    print(product)
+    continue
